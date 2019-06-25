5e912de28494bbc39c2adcc4269d32a0c701a727
daria <menscikova.daria@gmail.com>
2019-06-25 14:47:43 +0300

    all project bestgeek

---
+++ src/js/libs.js
@@ -0,0 +1,7 @@
+// =require libs/jquery-3.3.1.min.js
+// =require /libs/jquery-ui.min.js
+// =require /libs/swiper.min.js
+// =require /libs/validator.min.js
+// =require /libs/jquery.jeditable.min.js
+// =require /libs/jquery.jeditable.datepicker.min.js
+// =require /libs/chartjs.js
